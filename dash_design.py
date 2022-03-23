20d1e19c1661c7bfb10ca70690c1b4f656715c34
K1ddong <bw120811@naver.com>
2022-03-23 20:59:34 +0900

    대시보드 요소 초안

--- dash_design.py
+++ dash_design.py
@@ -2,6 +2,8 @@ from symtable import Symbol
 from click import style
 import pandas as pd
 
+from crawlers import google_trends
+
 
 google_rising = pd.read_csv('google_rising.csv')
 google_top = pd.read_csv('google_top.csv')
@@ -36,6 +38,11 @@ keyword = '밥솥'
 def brand_from_title(title):
     brand = title.split(' ')[0]
     return brand
+
+def strip_keyword(title):
+    keyword = 'rice cooker'
+    return title.replace(keyword, '').strip()
+
 
 
 ####---------네이버-----------###
@@ -183,13 +190,103 @@ graph_scatter_naver_item_price = px.scatter(naver_item_info,
                                             color='브랜드',
                                             color_discrete_map=naver_color, 
                                             symbol='브랜드').update_traces(marker_size=10)
-
+graph_scatter_naver_item_price.update_layout(showlegend=False)
 
 graph_pie_naver_brand_dist = px.pie(naver_item_info['브랜드'].value_counts(),
                                     values = naver_item_info['브랜드'].value_counts().values,
                                     names = naver_item_info['브랜드'].value_counts().index,
                                     color = naver_item_info['브랜드'].value_counts().index,
                                     color_discrete_map=naver_color)
+
+##----------------------------쇼피-----------------------------
+###쇼피 최소, 평균, 최대 가격
+
+shopee_item_info['브랜드'] = shopee_item_info['상품명'].apply(brand_from_title)
+
+shopee_avg_price = round(shopee_item_info['가격(RM)'].mean())
+card_shopee_avg_price = [
+    dbc.CardHeader("쇼피 top50 평균 가격"),
+    dbc.CardBody([html.H3(f"{shopee_avg_price}")])  
+    ]
+
+shopee_min_price = round(shopee_item_info['가격(RM)'].min())
+card_shopee_min_price = [
+    dbc.CardHeader("쇼피 top50 최저 가격"),
+    dbc.CardBody([html.H3(f"{shopee_min_price}")])  
+    ]
+
+shopee_max_price = round(shopee_item_info['가격(RM)'].max())
+card_shopee_max_price = [
+    dbc.CardHeader("쇼피 top50 최고 가격"),
+    dbc.CardBody([html.H3(f"{shopee_max_price}")])  
+    ]
+###쇼피 top50 기준 월 판매 시장 규모
+shopee_item_info['월판매액'] = shopee_item_info['가격(RM)'] * shopee_item_info['판매량(월 평균)']
+shopee_top50_monthly_market_share = shopee_item_info['월판매액'].sum()
+
+###쇼피 그래프 (가격대 스캐터, 브랜드 분포 파이)
+mask = shopee_item_info['브랜드'].map(shopee_item_info['브랜드'].value_counts()) == 1
+shopee_item_info['브랜드'] =  shopee_item_info['브랜드'].mask(mask, 'other')
+
+
+shopee_color = dict(zip(shopee_item_info["브랜드"].unique(), px.colors.qualitative.G10))
+
+graph_scatter_shopee_item_price = px.scatter(shopee_item_info, 
+                                            x='가격(RM)', 
+                                            y='판매량(월 평균)', 
+                                            hover_name='상품명', 
+                                            color='브랜드',
+                                            color_discrete_map=shopee_color, 
+                                            symbol='브랜드').update_traces(marker_size=10)
+graph_scatter_shopee_item_price.update_layout(showlegend=False)
+
+graph_pie_shopee_brand_dist = px.pie(shopee_item_info['브랜드'].value_counts(),
+                                    values = shopee_item_info['브랜드'].value_counts().values,
+                                    names = shopee_item_info['브랜드'].value_counts().index,
+                                    color = shopee_item_info['브랜드'].value_counts().index,
+                                    color_discrete_map=shopee_color)
+
+##-----------------------구글 키워드------------------------------------
+google_top10 = google_top.sort_values(by='가중치',ascending=False).head(10)
+
+###검색 추이 그래프
+graph_line_google_search_trend = px.line(naver_trend, x = '날짜', 
+                                                          y = 'Rice cooker', 
+                                                          title = '구글 키워드 검색 추이')
+
+###top 연관 검색어
+graph_bar_google_related_top = px.bar(google_top10, x = '연관 검색어',
+                                                  y = '가중치')
+
+google_keyword_related_words = google_top10['연관 검색어'].apply(strip_keyword).value_counts()
+
+graph_pie_google_keyword_dist = px.pie(google_keyword_related_words,
+                                    values = google_keyword_related_words.values,
+                                    names = google_keyword_related_words.index,
+                                    color = google_keyword_related_words.index)
+graph_pie_google_keyword_dist.update_traces(hoverinfo='label+percent', textinfo='value')
+
+
+###rising 연관 검색어
+graph_bar_google_related_rising = px.bar(google_rising, x = '연관 검색어',
+                                                  y = '가중치')
+
+graph_table_google_related_top = go.Figure(data=[go.Table(
+    header=dict(values=list(google_top10.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[google_top10['연관 검색어'], google_top10['가중치']],
+               fill_color='lavender',
+               align='left'))
+                ])
+graph_table_google_related_rising = go.Figure(data=[go.Table(
+    header=dict(values=list(google_rising.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[google_rising['연관 검색어'], google_rising['가중치']],
+               fill_color='lavender',
+               align='left'))
+                ])
 
 
 #------------------------대시보드 레이아웃 ----------------------------------
@@ -237,8 +334,39 @@ app.layout = html.Div(id = 'parent_div', children = [
                            'whiteSpace':'normal'},
                     fluid = False)
         ]),
-    html.Div(id = 'naver_item_price_scatter', children=[dcc.Graph(figure=graph_scatter_naver_item_price)]),
-    html.Div(id = 'naver_brand_dist_pie', children=[dcc.Graph(figure=graph_pie_naver_brand_dist)]),
+    html.Br(),
+    html.Div(id = 'shopee_item_price_card', children = [
+        dbc.Container([
+            dbc.Row([
+                dbc.Col(dbc.Card(card_shopee_min_price, color = 'primary', outline = True)),
+                dbc.Col(dbc.Card(card_shopee_avg_price, color = 'info', inverse = True)),
+                dbc.Col(dbc.Card(card_shopee_max_price, color = 'info', inverse = True))
+                ]),
+            dbc.Row([
+                dbc.Col(dcc.Graph(figure=graph_scatter_shopee_item_price)),
+                dbc.Col(dcc.Graph(figure=graph_pie_shopee_brand_dist))
+                ])
+            ],
+                    style={'textAlign':'center',
+                           'whiteSpace':'normal'},
+                    fluid = False)
+        ]),
+    html.Br(),
+    html.Div(id = 'google_search', children = [
+        dbc.Container([
+            dbc.Row([
+                dbc.Col(dcc.Graph(figure= graph_line_google_search_trend)),
+                dbc.Col(dcc.Graph(figure= graph_bar_google_related_top)),
+                ]),
+            dbc.Row([
+                dbc.Col(dcc.Graph(figure= graph_table_google_related_rising)),
+                dbc.Col(dcc.Graph(figure= graph_pie_google_keyword_dist))
+                ]),
+            ],
+                    style={'textAlign':'center',
+                           'whiteSpace':'normal'},
+                    fluid = False)
+    ]),
     html.Div(id = 'naver_item', children = [
         dbc.Container([
             dbc.Row([
@@ -255,4 +383,3 @@ app.layout = html.Div(id = 'parent_div', children = [
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-    print(naver_color)
